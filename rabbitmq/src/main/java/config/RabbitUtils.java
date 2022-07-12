23334a2859776a10a827beb369ec2e7e8ddbb0f4
Jy <1243871983@qq.com>
2022-07-12 21:44:02 +0800

    rabbitmq 增加两种工作队列 helloworld和workqueue模式

---
+++ rabbitmq/src/main/java/config/RabbitUtils.java
@@ -0,0 +1,23 @@
+package config;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class RabbitUtils {
+    static ConnectionFactory connectionFactory=new ConnectionFactory();
+    public static String QUEUE_HELLO_WORLD="helloworld";
+    public static ConnectionFactory createConnectionFactory(){
+        connectionFactory.setHost("127.0.0.1");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+    public static Connection getConnection() throws IOException, TimeoutException {
+        return RabbitUtils.createConnectionFactory().newConnection();
+    }
+}
