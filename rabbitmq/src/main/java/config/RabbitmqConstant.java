7b8d62c483d7013c6dfe96d58deed61913eceea0
Jy <1243871983@qq.com>
2022-08-01 20:06:16 +0800

    增加mq的 1.工作队列 2.发布订阅模式

---
+++ rabbitmq/src/main/java/config/RabbitmqConstant.java
@@ -0,0 +1,14 @@
+package config;
+
+/**
+ * rabbitmq常量
+ * @author Administrator
+ */
+public class RabbitmqConstant {
+    // 支付成功交换机
+    public static String EXCHANGE_PAY_SUCCESS="pay_success";
+    // 支付成功队列1
+    public static String QUEUE_PAY_1="pay_queue_1";
+    // 支付成功队列2
+    public static String QUEUE_PAY_2="pay_queue_2";
+}
---
+++ rabbitmq/src/main/java/publish/PayService.java
@@ -0,0 +1,32 @@
+package publish;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import config.RabbitUtils;
+import config.RabbitmqConstant;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 支付消息生产者
+ */
+public class PayService {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 建立TCP连接
+        Connection connection=RabbitUtils.getConnection();
+        // 创建通道
+        Channel channel = connection.createChannel();
+        while (true){
+            String input= new Scanner(System.in).next();
+
+            // 1.交换机名字
+            // 2.队列名字 在发布订阅模式下发布者不需要绑定队列 消费者自行绑定队列,队列绑定交换机来实现通信
+            channel.basicPublish(RabbitmqConstant.EXCHANGE_PAY_SUCCESS,"",false,null,input.getBytes());
+//        channel.close();
+//        connection.close();
+            System.out.println("数据发送成功");
+        }
+    }
+}
