48b9af9c06a98535494b3e03fbe449bf28e30c79
Jy <1243871983@qq.com>
2022-08-04 20:45:15 +0800

    1

--- producer-rabbitmq/src/main/java/jy/controller/Producer.java
+++ producer-rabbitmq/src/main/java/jy/controller/Producer.java
@@ -1,13 +1,22 @@
 package jy.controller;
 
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/produce")
 public class Producer {
 
     @Autowired
-    AmqpTemplate amqpTemplate;
+    RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/pub1")
+    public void pub1(){
+
+    }
 }
